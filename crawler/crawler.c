e9789e8a9f13843052f243f6a96a83aeba37adb7
evejcik <f007017@hub5.thayer.dartmouth.edu>
2023-10-16 20:59:29 -0400

    Trying to solve memory leaks

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -11,6 +11,13 @@
 
 #include <stdio.h>
 #include "webpage.h"
+#include <stdlib.h>                                                                                 
+#include "queue/queue.h"
+
+void printData(void* elementp) {                                                                    
+    printf("Data: %d\n", *(char*)elementp);                                                          
+}                                                                                                   
+      
 
 int main(int argc, char* argv[]){
 	//printf("hello");
@@ -18,24 +25,33 @@ int main(int argc, char* argv[]){
 	webpage_t* page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
 
 	if(!webpage_fetch(page)){
+		webpage_delete(page);
 		exit(EXIT_FAILURE);
 	};
 
 	int pos = 0;
 	char *result;
+	//queue_t* myQueue = qopen();
 
 	if(webpage_fetch(page)){
 		//char *html = webpage_getHTML(page);
 		while((pos = webpage_getNextURL(page, pos, &result)) > 0) {
 			printf("Found url: %s\n", result);
 			if(IsInternalURL(result)){
-					printf("Is Internal.\n");
-				}else {
-					printf("Is External.\n");
+				printf("Is Internal.\n");
+				//	qput(myQueue, result);
+			}else{
+				printf("Is External.\n");
+				webpage_delete(page);
 			}
 			free(result);
 		}
+	}else{
+		webpage_delete(page);
 	}
+
+	//qapply(myQueue, printData);
+	//qclose(myQueue);
 
 	webpage_delete(page);
 

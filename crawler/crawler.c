3e36318e7c25bcd1feb4008e92e654fcb0be139c
evejcik <f007017@hub5.thayer.dartmouth.edu>
2023-10-17 14:01:40 -0400

    Q4

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -10,56 +10,71 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <webpage.h>
 #include <stdlib.h>
 #include <hash.h>       
 #include <queue.h>
+
+#define MAX_VISITED_URLS 1000
 
 void printURL(void *data) {
         webpage_t *page = (webpage_t *)data;
         char *url = webpage_getURL(page);
 				webpage_delete(url);
         printf("%s\n", url);
-    }
+}
+
+bool searchURL(void *elementp, const void *key) {
+    char *url = webpage_getURL((webpage_t *)elementp);
+		webpage_delete(url);
+    return (strcmp(url, (char *)key) == 0);
+}
       
 
 int main(){
-	
+	// Create an array or list to track visited URLs
+	//	char *visitedURLs[MAX_VISITED_URLS];
 	//printf("hello");
 
 	char *seedURL = "https://thayer.github.io/engs50/";
 	webpage_t* page = webpage_new(seedURL, 0, NULL);
 
-	/*if(!webpage_fetch(page)){
-		webpage_delete(page);
-		exit(EXIT_FAILURE);
-		};*/
-
 	int pos = 0;
 	char *result;
 	queue_t* myQueue = qopen();
+	hashtable_t *visitedURLs = hopen(100); 
 
 	if(webpage_fetch(page)){
-		//char *html = webpage_getHTML(page);
 		while((pos = webpage_getNextURL(page, pos, &result)) > 0) {
-			//printf("Found url: %s\n", result);
 			if(IsInternalURL(result)){
-				webpage_t *internalPage = webpage_new(result, 0, NULL);
-				//printf("Is Internal.\n");
-				qput(myQueue, internalPage);
-			}//else{
-				//	printf("Is External.\n");
-				//webpage_delete(page);
-			//}
+				if(hsearch(visitedURLs, searchURL, result, strlen(result)) == NULL){
+					webpage_t *internalPage = webpage_new(result, 0, NULL);
+					qput(myQueue, internalPage);
+					hput(visitedURLs, internalPage, result, strlen(result));
+					
+				}
+			}
 			free(result);
+			//qclose(myQueue);
 		}
-		//free(result);
-		qapply(myQueue,printURL);
+		//qapply(myQueue,printURL);
+		//qclose(myQueue);
+		//		happly(visitedURLs, printURL);
+		//		printf("Unique URLs in myQueue:\n");
+		//		qapply(myQueue, printURL);
+		
+		// Print the visited URLs stored in the hashtable
+		printf("Visited URLs:\n");
+		happly(visitedURLs, printURL);
 		qclose(myQueue);
+		hclose(visitedURLs);
 		webpage_delete(page);
 		exit(EXIT_SUCCESS);
 	}else{
 		qclose(myQueue);
+		hclose(visitedURLs);
+		webpage_delete(page);
 		exit(EXIT_FAILURE);
 	}
 

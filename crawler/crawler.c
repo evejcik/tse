9b5bdeebf01fe8513b2bdd6c62c522ba3db12e3d
evejcik <f007017@hub5.thayer.dartmouth.edu>
2023-10-18 14:09:32 -0400

    From Matthew - q4,q5

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,97 +1,87 @@
-/* 
- * crawler.c --- 
- * 
- * Author: Emma Vejcik
- * Created: 10-15-2023
- * Version: 1.0
- * 
- * Description: 
- * 
- */
-
-#include <stdio.h>
-#include <string.h>
-#include <webpage.h>
-#include <stdlib.h>
-#include <hash.h>       
-#include <queue.h>
-
-//#define MAX_VISITED_URLS 1000
-
-void printURL(void *data) {
-        webpage_t *page = (webpage_t *)data;
-        char *url = webpage_getURL(page);
-				//				strcpy(url, url2);
-				//	webpage_delete(url);
-        printf("%s\n", url);
-				free(url);
-				//webpage_delete(url);
-}
-
-bool searchURL(void *elementp, const void *key) {
-    char *url = webpage_getURL((webpage_t *)elementp);
-		bool test;
-		return (strcmp(url, (char *) key) == 0);
-		/*		if(strcmp(url, (char *) key) == 0){
-			test = true;
-		}else{
-			test = false;
-		}
-		free(url);
-		return test;;*/
-		/*		cmp = (strcmp(url, (char *)key) == 0);
-		free(url);
-
-		if(cmp == 0){
-			return true;
-		}else{
-			return false;
-			}*/
-}
-      
-
-int main(){
-	// Create an array or list to track visited URLs
-	//	char *visitedURLs[MAX_VISITED_URLS];
-	//printf("hello");
-
-	char *seedURL = "https://thayer.github.io/engs50/";
-	webpage_t* page = webpage_new(seedURL, 0, NULL);
-
-	int pos = 0;
-	char *result;
-	queue_t* myQueue = qopen();
-	hashtable_t *visitedURLs = hopen(100); 
-
-	if(webpage_fetch(page)){
-		while((pos = webpage_getNextURL(page, pos, &result)) > 0) {
-			if(IsInternalURL(result)){
-				if(hsearch(visitedURLs, searchURL, result, strlen(result)) == NULL){
-					webpage_t *internalPage = webpage_new(result, 0, NULL);
-					qput(myQueue, internalPage);
-					hput(visitedURLs, internalPage, result, strlen(result));
-					
-				}
-			}
-			free(result);
-			//qclose(myQueue);
-		}
-		
-		// Print the visited URLs stored in the hashtable
-		printf("Visited URLs:\n");
-		happly(visitedURLs, printURL);
-		//qclose(myQueue);
-		//hclose(visitedURLs);
-
-		//		qclose(myQueue);
-		webpage_delete(page);
-		hclose(visitedURLs);
-		//qclose(myQueue);
-		exit(EXIT_SUCCESS);
-	}else{
-		//qclose(myQueue);
-		hclose(visitedURLs);
-		webpage_delete(page);
-		exit(EXIT_FAILURE);
-	}
-}
+/*                                                                                                
+ * crawler.c ---                                                                                  
+ *                                                                                                
+ * Author: Emma Vejcik                                                                            
+ * Created: 10-15-2023                                                                            
+ * Version: 1.0                                                                                   
+ *                                                                                                
+ * Description:                                                                                   
+ *                                                                                                
+ */                                                                                               
+                                                                                                  
+#include <stdio.h>                                                                                
+#include <string.h>                                                                               
+#include <webpage.h>                                                                              
+#include <stdlib.h>                                                                               
+#include <hash.h>                                                                                 
+#include <queue.h>                                                                                
+                                                                                                  
+//#define MAX_VISITED_URLS 1000                                                                   
+                                                                                                  
+bool searchURL(void *elementp, const void *key) {                                                \
+char *url = (char *)elementp;                                                                     
+  return(strcmp(url, (char *) key) == 0);                                                         
+}                                                                                                 
+void printURL(void *data) {                                                                       
+        webpage_t *page = (webpage_t *)data;                                                      
+        char *url = webpage_getURL(page);                                                         
+        printf("%s\n", url);                                                                      
+}                                                                                                 
+                                                                                                  
+int32_t pagesave(webpage_t *pagep, int id, char *dirname){                                        
+  FILE * resultFile;                                                                              
+  char filename[50];                                                                              
+                                                                                                  
+  sprintf(filename, "../%s/%d", dirname, id);                                                     
+  resultFile = fopen(filename, "w");                                                              
+                                                                                                  
+  if(resultFile != NULL){                                                                         
+    fprintf(resultFile, "%s \n %d \n %d \n %s", webpage_getURL(pagep), webpage_getDepth(pagep), w\
+ebpage_getHTMLlen(pagep), webpage_getHTML(pagep));                                                
+    fclose(resultFile);                                                                           
+    return 0;                                                                                     
+  }                                                                                               
+  else{                                                                                           
+    return 1;                                                                                     
+  }                                                                                               
+}                                                                                                 
+                                                                                                  
+                                                                                                  
+int main(){                                                                                       
+  char *seedURL = "https://thayer.github.io/engs50/";                                             
+  webpage_t* page = webpage_new(seedURL, 0, NULL);                                                
+  webpage_t *internalPage;                                                                        
+  queue_t *myQueue = qopen();;                                                                    
+  int pos = 0;                                                                                    
+  char *result;                                                                                   
+  hashtable_t *visitedURLs = hopen(100);                                                          
+                                                                                                  
+                                                                                                  
+  if(webpage_fetch(page)){                                                                        
+                                                                                                  
+    pagesave(page, 1, "pages");                                                                   
+                                                                                                  
+    while((pos = webpage_getNextURL(page, pos, &result)) > 0) {                                   
+      if(IsInternalURL(result) && hsearch(visitedURLs, searchURL, result, strlen(result)) == NULL\
+){                                                                                                
+           internalPage = (webpage_new(result, 0, NULL));                                         
+           qput(myQueue, internalPage);                                                           
+           hput(visitedURLs, result, result, strlen(result));                                     
+      }                                                                                           
+      else{                                                                                       
+        free(result);                                                                             
+      }                                                                                           
+    }                                                                                             
+   printf("Visited URLs:\n");                                                                     
+   qapply(myQueue, printURL);                                                                     
+                                                                                                  
+   while((internalPage = (qget(myQueue))) != NULL){                                               
+        webpage_delete(internalPage);                                                             
+      }                                                                                           
+      qclose(myQueue);                                                                            
+      hclose(visitedURLs);                                                                        
+      webpage_delete(page);                                                                       
+  }                                                                                     
+    exit(EXIT_FAILURE);                                                                           
+}                                                                                                 
+                                                                                                  

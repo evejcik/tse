fe28f146bb1bbe7de82fbd46a8aa0464b3e4a3e8
evejcik <f007017@hub5.thayer.dartmouth.edu>
2023-10-24 19:24:02 -0400

    Starting indexer

---
+++ indexer/indexer.c
@@ -0,0 +1,26 @@
+/* 
+ * indexer.c --- 
+ * 
+ * Author: Emma Vejcik
+ * Created: 10-24-2023
+ * Version: 1.0
+ * 
+ * Description: 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <webpage.h>
+#include <queue.h>
+#include <hash.h>
+#include <unistd.h>
+#include <sys/stat.h>  
+
+int main(int argc, char *argv[]){
+	printf("hello");
+
+}

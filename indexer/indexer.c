4648a8d1347869d235cd100e2de25854535d80db
evejcik <f007017@hub5.thayer.dartmouth.edu>
2023-10-25 21:11:42 -0400

    Working on Step 3

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -21,20 +21,39 @@
 #include <sys/stat.h>  
 #include <pageio.h>
 
-
-void NormalizeWord(char* word){                                                   if (strlen(word) < 3){                                                           return false;                                                                }                                                                            
-	if(!isalpha(word[i])){                                                     
-      return false;                                                            
+//Step 2
+void NormalizeWord(char* word){
+	if (strlen(word) < 3){
+    return false;
 	}
-	word[i] = tolower(word[i]);
 	
+	if(!isalpha(word[i])){                                                     
+		return false;                                                            
+	}
+	word[i] = tolower(word[i]);	
 }
-	 
+
+
+//Step 3: Hash Table of Words
+void updateWordCount(hashtable_t *htp, const char *word) {
+	word_count_t *entry = (word_count_t *)hsearch(htp, NULL, word, strlen(word));
+	if (entry == NULL) {
+		// Word not found, create a new entry
+		entry = (word_count_t *)malloc(sizeof(word_count_t));
+		strncpy(entry->word, word, sizeof(entry->word));
+		entry->count = 1;
+		hput(htp, entry, word, strlen(word));
+	} else {
+		// Word found, update the count
+		entry->count++;
+	}
+}
+
 
 int main(int argc, char *argv[]){
 	//char* pages[200];
 
-	printf("hello");
+	//	printf("hello");
 	webpage_t* page = pageload(1, "pages");
 
 	int pos = 0;

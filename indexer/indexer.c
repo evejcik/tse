c40981387a0ca8cb2566e9f83f8e0abf28161c6e
evejcik <f007017@hub5.thayer.dartmouth.edu>
2023-10-28 18:06:32 -0400

    Question 2, 3, not sure about 4

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -2,99 +2,177 @@
  * indexer.c --- 
  * 
  * Author: Emma Vejcik
- * Created: 10-24-2023
+ * Created: 10-28-2023
  * Version: 1.0
  * 
  * Description: 
  * 
  */
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <webpage.h>
-#include <queue.h>
-#include <hash.h>
-#include <unistd.h>
-#include <sys/stat.h>  
-#include <pageio.h>
-#include <ctype.h>
-
-//Step 2
-void NormalizeWord(char* word){
-	if (strlen(word) < 3){
-    return false;
-	}
-
-	for(int i = 0; word[i] != '\0'; i++){
-		if(!isalpha(word[i])){
-			return false;
-		}
-		word[i] = tolower(word[i]);	
-	}
-}
-
-
-typedef struct word_count {
-    char word[200];  // Adjust the size as needed
-    int count;
-} word_count_t;
-
-//Step 3: Hash Table of Words
-void updateWordCount(hashtable_t *htp, const char *word) {
-	word_count_t *entry = (word_count_t *)hsearch(htp, NULL, word, strlen(word));
-	if (entry == NULL) {
-		// Word not found, create a new entry
-		entry = (word_count_t *)malloc(sizeof(word_count_t));
-		strncpy(entry->word, word, sizeof(entry->word));
-		entry->count = 1;
-		hput(htp, entry, word, strlen(word));
-	} else {
-		// Word found, update the count
-		entry->count++;
-	}
-}
-
-//Step 3 - sum the counts
-/*void countSumCallback(void* ep) {
-	int* total_count = (int*)arg;
-	*total_count += ((word_count_t*)ep)->count;
-	}*/
-
-int main(int argc, char *argv[]){
-	//char* pages[200];
-
-	//	printf("hello");
-	webpage_t* page = pageload(1, "pages");
-
-	int pos = 0;
-	//	char *result = NULL;
-
-	char *word = NULL;
-	//	hashtable_t *htp = hopen(1000000);
-	//	int pos = 0;
-		
-	// Loop to extract and print words
-	while (webpage_getNextWord(page, pos, &word) > 0) {
-		NormalizeWord(word);
-		printf("%s\n", word);  // Print the extracted word
-		free(word);  // Free the memory allocated by webpage_getNextWord
-	}
-
-	int total_count = 0;
-	/*	happly(htp, countSumCallback, &total_count);
-	
-	// Print the total count
-	printf("Total Count: %d\n", total_count);
-	
-	// Free the hashtable when you're done
-	happly(htp, free);
-	hclose(htp);*/
-	
-	
-	// Free the webpage when you're done
-	webpage_delete(page);
-	
-}
+#include <stdio.h>                                                                     
+#include <stdlib.h>                                                                    
+#include "webpage.h"                                                                   
+#include "pageio.h"                                                                    
+#include <string.h>                                                                    
+#include <ctype.h>                                                                     
+#include <string.h>                                                                    
+#include <stdbool.h>                                                                   
+#include "hash.c"                                                                      
+                                                                                       
+typedef struct indexEntry{                                                             
+  char* givenWord;                                                                     
+  int count;                                                                           
+}indexEntry_t;                                                                         
+                                                                                       
+bool matchingWordsSearch(void* elementp, const void* searchkeyp) {                     
+  indexEntry_t *givenEntry = (indexEntry_t*) elementp;                                 
+  const char *key = (const char*) searchkeyp;                                          
+  if(elementp == NULL) {                                                               
+    return false;                                                                      
+  }                                                                                    
+  if(strcmp((givenEntry -> givenWord), key) == 0) {                                    
+    return true;                                                                       
+  }                                                                                    
+  return false;                                                                        
+}                                                                                      
+                                                                                       
+                                                                                       
+void printh(void* elementp) {                                                          
+  indexEntry_t *entry = (indexEntry_t*) elementp;                                      
+  char* printWord = entry -> givenWord;                                                
+  int printCount = entry -> count;                                                     
+  printf("%s , %d \n ", printWord, printCount);                                        
+}                                                                                      
+                                                                                       
+char* NormalizeWord(char* word){                                                       
+  int cCounter;                                                                        
+  char c;                                                                              
+  // Check is word is greater than 3 characters                                        
+  int wordLen = strlen(word);                                                          
+  if (wordLen < 3){                                                                    
+    return NULL;                                                                       
+  }                                                                                    
+                                                                                       
+  // Check is word contains alphanumeric characters                                    
+  for ( cCounter = 0; cCounter < wordLen; cCounter++){                                 
+    c = word[cCounter];                                                                
+    if ( (isalpha(c)) == 0 ){                                                          
+      return NULL;                                                                     
+    }                                                                                  
+  }                                                                                    
+  // Check if word  is uppercase                                                       
+  for ( cCounter = 0; cCounter < wordLen; cCounter++){                                 
+    c = tolower(word[cCounter]);                                                       
+    word[cCounter] = c;                                                                
+  }                                                                                    
+  return word;                                                                         
+}                                                                                      
+                                                                                       
+int total = 0;                                                                         
+void wordCounter(void* ep) {                                                           
+  indexEntry_t *element = (indexEntry_t*) ep;                                          
+  if (element == NULL) {                                                               
+    return;                                                                            
+  }                                                                                    
+  total = total + (element -> count);                                                  
+}                                                                                      
+                                                                                       
+void freeHashWords(void *ep) {                                                         
+  indexEntry_t* entry  = ep;                                                           
+  char* word = entry -> givenWord;                                                     
+  free(word);                                                                          
+  free(entry);                                                                         
+}                                                                                      
+                                                                                       
+                                                                                       
+int main (void){                                                                       
+  char* word;                                                                          
+  int pos=0;                                                                           
+  hashtable_t* indexHT;                                                                
+  webpage_t* newWebPg;                                                                 
+                                                                                       
+  //open hashtable                                                                     
+  indexHT = hopen(150);                                                                
+                                                                                       
+  // Load webpage                                                                      
+  newWebPg = pageload(1,"../test");                                                    
+                                                                                       
+  FILE *fp = fopen("../test/1","r");                                                   
+  if (fp == NULL){                                                                     
+    printf("Error opening the file \n");                                               
+    return 0;                                                                          
+  }                                                                                    
+  // Get next word in HTML                                                             
+  while (feof(fp)== 0){                                                                
+    pos = webpage_getNextWord(newWebPg, pos, &word);                                   
+    if (word == NULL){                                                                 
+      free(word);                                                                      
+      break;                                                                           
+    }                                                                                  
+    word = NormalizeWord(word);                                                        
+    if (word == NULL){                                                                 
+      free(word);                                                                      
+      pos++;                                                                           
+    }                                                                                  
+    else {                                                                             
+      indexEntry_t *newEntry = malloc(sizeof(indexEntry_t));                           
+      if (newEntry != NULL) {                                                          
+        newEntry -> givenWord = word;                                                  
+        newEntry -> count = 1;                                                         
+      }                                                                                
+      else {                                                                           
+        printf("memory error \n");                                                     
+        return 1;                                                                      
+      }                                                                                
+                                                                                       
+      indexEntry_t *hfound = (indexEntry_t*) hsearch(indexHT, matchingWordsSearch, wor\
+d, strlen(word));                                                                      
+      if (hfound == NULL) {                                                            
+        hput(indexHT, newEntry, word, strlen(word));                                   
+      }                                                                                
+                                                                                       
+      else {                                                                           
+        hfound -> count = (hfound -> count) + 1;                                       
+                                                                                       
+      }                                                                                
+      pos++;                                                                           
+    }                                                                                  
+  }                                                                                    
+                                                                                       
+  //free(newWebPg->url);                                                               
+  //free(newWebPg->html);                                                              
+  free(newWebPg);                                                                      
+  fclose(fp);                                                                          
+  happly(indexHT, printh);                                                             
+  happly(indexHT, wordCounter);                                                        
+  printf("Total # of Words: %d\n", total);                                             
+  happly(indexHT, freeHashWords);                                                      
+  hclose(indexHT);                                                                     
+  free(word);                                                                          
+                                                                                       
+                                                                                       
+  return 1;                                                                            
+}                                                                                      
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                    
